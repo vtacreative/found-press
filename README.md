566950f9869eb61a46b1aa6ccc51723473f04630
CL75 <chris@cclsf.com>
2013-01-25 16:31:40 -0800

    add html5 boilerplate and foundation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-A Foundation-based TwentyTwelve child theme for WordPress.
+A Foundation 3.2 -based TwentyTwelve child theme for WordPress.
+
+The idea is to preserve the interesting 2012 WordPress functionality while
+abandoning all of the thematic and architectural elements so we can replace 
+them with customized Foundation architecture.
+
+:MORE
+http://foundation.zurb.com/
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="app/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+<script src="<?php echo JS; ?>plugins.js"></script>
+<script src="<?php echo JS; ?>main.js"></script>
+
+<!-- Analytics: change UA-XXXXX-X to be your code. -->
+<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
