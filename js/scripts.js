1ecc91afb2e241e83588603a4e33d96a6b2424e9
CL75 <chris@cclsf.com>
2013-01-25 21:10:59 -0800

    found-press up and running

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="app/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
 <script src="<?php echo JS; ?>plugins.js"></script>
-<script src="<?php echo JS; ?>main.js"></script>
 
 <!-- Analytics: change UA-XXXXX-X to be your code. -->
 <script>
