8d19142f2922c6558178cb419b618951d1c1d811
CL75 <chris@cclsf.com>
2013-02-03 15:00:13 -0800

    some cleanup

--- page-templates/single-events.php
+++ page-templates/single-events.php
@@ -3,27 +3,29 @@
 get_header(); ?>
 
 	<div id="primary">
-		<div class="row a" role="main">
+		<div class="row" role="main">
 
 			<?php
 			$mypost = array('post_type' => 'events', );
 			$loop = new WP_Query($mypost);	
 			while($loop->have_posts()): $loop->the_post();
 			?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header>
-					<?php the_post_thumbnail(array(100, 100)); ?>
-					<strong>Name: </strong><?php the_title(); ?><br />
-					<strong>Desc: </strong>
-					<?php echo esc_html(get_post_meta(get_the_ID(), 'event_desc', true)); ?>
-				</header>
-
-				<!-- Display movie review contents -->
-				<div><?php the_content(); ?></div>
-			</article>
+			<div class="four columns panel end">
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header>
+						<?php the_post_thumbnail(array(100, 100)); ?>
+						<strong>Name: </strong>
+						<?php echo esc_html(get_post_meta(get_the_ID(), 'event_name', true)); ?>
+						<strong>Desc: </strong>
+						<?php echo esc_html(get_post_meta(get_the_ID(), 'event_desc', true)); ?>
+					</header>
+					<?php the_title(); ?>
+					<?php the_content(); ?>
+				</article>
+			</div>
 			<?php endwhile; ?>
 		</div>
-</div>
+	</div>
 
 <?php wp_reset_query(); ?>
 <?php get_footer(); ?>
