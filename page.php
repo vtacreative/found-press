c00cc80c913ab1949b524553918d093a2a122e34
CL75 <funnotfame@gmail.com>
2013-02-27 20:17:04 -0800

    remove sidebar from default page view

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php
+/* Displays content when nothing more specific matches a query.
+   http://codex.wordpress.org/Template_Hierarchy */
+get_header(); ?>
+		<div class="row" role="main">
+			<div class="twelve columns">
+				<?php if(have_posts()): // start loop if posts exist ?>
+					<?php while(have_posts()): the_post(); ?>
+						<?php get_template_part('content', get_post_format()); ?>
+					<?php endwhile; ?>
+				<?php endif; ?><!--/have_posts()-->
+			</div>
+		</div><!--/.row -->
+<?php get_footer(); ?>
