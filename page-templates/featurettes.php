f1192d85fb468d6be9057a6e65fe49fd12e77ce4
CL75 <funnotfame@gmail.com>
2013-04-13 21:34:55 -0700

    add push / pull to featurettes template

--- page-templates/featurettes.php
+++ page-templates/featurettes.php
@@ -9,21 +9,21 @@ get_header(); ?>
 			</div>
 			<div class="large-7 columns">
 				<h1>Super Groovy Headline</h1>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. <a href="#">Learn More &rarr;</a></p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue.<br><a href="#">Learn More &rarr;</a></p>
 			</div>
-		</div><!--/.row-->
+		</div><!--/.row1-->
 		
 		<div class="row featurettes">
-			<div class="large-7 columns text-right">
-				<h1>Stellar Supporting Blurb</h1>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. <a href="#">Learn More &rarr;</a></p>
-			</div>
-			<div class="large-5 columns">
+			<div class="large-5 push-7 columns">
 				<div class="box effect4">
 					<img src="http://lorempixel.com/320/240/nightlife">
 				</div>
 			</div>
-		</div><!--/.row-->
+			<div class="large-7 pull-5 columns">
+				<h1>Stellar Supporting Blurb</h1>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue.<br><a href="#">Learn More &rarr;</a></p>
+			</div>
+		</div><!--/.row2-->
 		
 		<div class="row featurettes">
 			<div class="large-5 columns">
@@ -33,21 +33,20 @@ get_header(); ?>
 			</div>
 			<div class="large-7 columns">
 				<h1>Captivating Complementary Piece</h1>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. <a href="#">Learn More &rarr;</a></p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue.<br><a href="#">Learn More &rarr;</a></p>
 			</div>
-		</div><!--/.row-->
+		</div><!--/.row3-->
 				
 		<div class="row featurettes">
-			<div class="large-7 columns text-right">
-				<h1>Obligatory Closing Pitch</h1>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. <a href="#">Learn More &rarr;</a></p>
-			<a class="callout-link" href="#">Perfectly Placed Call To Action ></a>
-			</div>
-			<div class="large-5 columns">
+			<div class="large-5 push-7 columns">
 				<div class="box effect7">
 					<img src="http://lorempixel.com/320/240/city">
 				</div>
 			</div>
-		</div><!--/.row-->
-
+			<div class="large-7 pull-5 columns">
+				<h1>Obligatory Closing Pitch</h1>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue.<br><a href="#">Learn More &rarr;</a></p>
+			<a class="callout-link" href="#">Perfectly Placed Call To Action ></a>
+			</div>
+		</div><!--/.row4-->
 <?php get_footer(); ?>
