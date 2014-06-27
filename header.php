43a523da46cbb9e1a74340a374693e528d67d444
CL75 <funnotfame@gmail.com>
2014-06-27 09:51:12 -0700

    refactored header.php to better reflect WP coding standards

--- header.php
+++ header.php
@@ -7,10 +7,14 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
+	
 	<meta charset="utf-8">
+	
 	<title>
+		
 	<?php
 		// dynamic title = page title
+		// TODO: extract into a function or include file
 		global $page, $paged; 
 		wp_title('|', true, 'right');
 		bloginfo('name');
@@ -24,13 +28,21 @@
 		if($paged >= 2 || $page >= 2)
 		echo ' | ' . sprintf(__('Page %s'), max($paged, $page));
 	?>
+	
 	</title>
+	
 	<meta name="description" content="Your company description">
+	
 	<meta name="keywords" content="Your, Company, Keywords">
+	
 	<meta name="viewport" content="width=device-width">
+	
 	<link rel="shortcut icon" href="favicon.ico">
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	
 	<?php wp_head(); ?>
 	
 	<!-- POLYFILLS TO ADDRESS IE8's LACK OF MEDIA QUERY SUPPORT
@@ -52,26 +64,57 @@
 	</script>-->
 	
 </head>
+
 <body <?php body_class(); ?>>
+	
 	<a id="top"></a>
+	
 	<div id="page" class="hfeed">
+		
 		<div class="header-container">
+			
 			<div class="row">
+				
 	      <header class="large-4 columns" role="banner">
-					<h1 class="logo"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-					<?php if(bloginfo('description')) : ?>
-					<h2><?php bloginfo('description'); ?></h2>
+		
+					<h1 class="logo">
+						
+						<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						
+					</h1>
+					
+					<?php if ( bloginfo( 'description' ) ) : ?>
+						
+					<h2>
+					
+						<?php bloginfo( 'description' ); ?>
+					
+					</h2>
+					
 					<?php endif; ?>
+					
 				</header>
+				
 
 				<nav class="large-8 columns">
+					
 					<!-- remove 'text-right' class to float primary navigation back to the left -->
-					<?php wp_nav_menu(array('theme_location' => 'header-menu', 'container_class' => 'nav table text-right', 'sort_column' => 'menu_order')); ?>
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'nav table text-right', 'sort_column' => 'menu_order' ) ); ?>
+					
 				</nav>
 
 				<?php $header_image = get_header_image();
-				if(!empty($header_image)): ?>
-					<a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo esc_url($header_image); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+				
+				if ( !empty ( $header_image ) ) : ?>
+				
+					<a href="<?php echo esc_url(home_url('/')); ?>">
+						
+						<img src="<?php echo esc_url($header_image); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="found-press. get started fast with foundation 5">
+						
+					</a>
+					
 				<?php endif; ?>
+				
 			</div>
+			
 		</div><!--/.header-container-->
