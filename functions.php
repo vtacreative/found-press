f0196d3686cdf4885e54b84494682a09ae91ef88
CL75 <funnotfame@gmail.com>
2014-07-02 23:13:11 -0700

    modify update_post_meta function in functions.php

--- functions.php
+++ functions.php
@@ -194,13 +194,14 @@ update_option( 'permalink_structure', '/%postname%/' );
 
 /*	UPDATE THE DEFAULT SAMPLE PAGE TO FOUND-PRESS HOMEPAGE */
 $fp_homepage = array(
+	'ID'						=> 2,
 	'post_type'			=> 'page',
 	'post_name'			=> 'home',
 	'post_status'		=> 'publish',
   'post_title'    => 'Home',
-	'page_template'	=> 'featurettes.php'
 );
 wp_insert_post( $fp_homepage );
+update_post_meta( 2, '_wp_page_template', 'page-templates/featurettes.php' );
 
 /*	MAKE HOMEPAGE DISPLAY STATIC PAGE */
 update_option( 'show_on_front', 'page' );
