e475635478936cbd9372311c34f99c5a464792ad
CL75 <funnotfame@gmail.com>
2014-08-20 11:33:25 -0700

    deleted update options code from functions.php

--- functions.php
+++ functions.php
@@ -186,29 +186,3 @@ function clear_nav_menu_item_class($classes, $item, $args) {
     return array();
 }
 
-
-switch_theme( 'fp-child' );
-
-/*	SET PERMALINKS TO POST NAME */
-update_option( 'permalink_structure', '/%postname%/' );
-
-/*	UPDATE THE DEFAULT SAMPLE PAGE TO FOUND-PRESS HOMEPAGE */
-$fp_homepage = array(
-	'ID'						=> 2,
-	'post_type'			=> 'page',
-	'post_name'			=> 'home',
-	'post_status'		=> 'publish',
-  'post_title'    => 'Home',
-);
-wp_insert_post( $fp_homepage );
-update_post_meta( 2, '_wp_page_template', 'page-templates/featurettes.php' );
-
-/*	MAKE HOMEPAGE DISPLAY STATIC PAGE */
-update_option( 'show_on_front', 'page' );
-
-/*	DISPLAY THE SAMPLE PAGE */
-update_option( 'page_on_front', 2);
-
-
-
-
