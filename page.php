2f492afedb4c9a3bf7a966ed32bb47b6e579322b
CL75 <funnotfame@gmail.com>
2014-08-25 19:11:46 -0700

    updated page.php for foundation 5+

--- page.php
+++ page.php
@@ -2,16 +2,23 @@
 /* Displays content when nothing more specific matches a query.
    http://codex.wordpress.org/Template_Hierarchy */
 get_header(); ?>
+
 		<div class="row" role="main">
-			<div class="nine columns page">
+			
+			<div class="large-12 columns c">
+				
 				<?php if(have_posts()): // start loop if posts exist ?>
+					
 					<?php while(have_posts()): the_post(); ?>
+						
 						<?php get_template_part('content', get_post_format()); ?>
+						
 					<?php endwhile; ?>
+					
 				<?php endif; ?><!--/have_posts()-->
+				
 			</div>
-			<div class="three columns">
-				<?php get_sidebar(); ?>
-			</div>
+
 		</div><!--/.row -->
+		
 <?php get_footer(); ?>
