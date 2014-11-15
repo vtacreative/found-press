1038b727e3d84ba1d9e4c0584d69e2672493c776
CL75 <funnotfame@gmail.com>
2014-11-15 08:21:29 -0800

    remove Foundation markup from page.php since content.php brings in Foundation markup

--- page.php
+++ page.php
@@ -2,23 +2,15 @@
 /* Displays content when nothing more specific matches a query.
    http://codex.wordpress.org/Template_Hierarchy */
 get_header(); ?>
-
-		<div class="row" role="main">
+				
+	<?php if(have_posts()): // start loop if posts exist ?>
+		
+		<?php while(have_posts()): the_post(); ?>
 			
-			<div class="large-12 columns c">
-				
-				<?php if(have_posts()): // start loop if posts exist ?>
-					
-					<?php while(have_posts()): the_post(); ?>
-						
-						<?php get_template_part('content', get_post_format()); ?>
-						
-					<?php endwhile; ?>
-					
-				<?php endif; ?><!--/have_posts()-->
-				
-			</div>
-
-		</div><!--/.row -->
+			<?php get_template_part('content', get_post_format()); ?>
+			
+		<?php endwhile; ?>
+		
+	<?php endif; ?><!--/have_posts()-->
 		
 <?php get_footer(); ?>
