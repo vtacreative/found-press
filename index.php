d9011c4369682376f6a0311c39781efdadc4eac5
CL75 <funnotfame@gmail.com>
2015-01-08 16:03:06 -0800

    remove conditional logic from index.php; revert to default slideshow for now

--- index.php
+++ index.php
@@ -3,24 +3,6 @@
    http://codex.wordpress.org/Template_Hierarchy */
 get_header(); ?>
 
-	<?php if(is_home()): ?>
-			
-		<div id="birthfit" class="row full-width">
-		
-			<div class="medium-4 medium-offset-6 columns">
-
-			  <h3 style="color:white;padding-top:14px;">Brianna Battles, MS, CSCS</h3>
-	
-				<p>Brianna is a strength, conditioning and wellness coach who specializes in women's health and fitness.  She facilitates an empowering, supportive environment that educates and provides all-encompassing lifestyle wellness.</p>
-	
-				<img width="110" src="/wp-content/themes/fp-child/img/birthfit-logo.jpg">&nbsp;&nbsp;&nbsp;&nbsp;<a class="button tiny" href="/coaching-services/">Learn More &rarr;</a>
-				
-			</div>
-			
-		</div>
-
-	<?php endif; ?>
-
 	<?php if(have_posts()): // start loop if posts exist ?>
 		
 		<?php while(have_posts()): the_post(); ?>
