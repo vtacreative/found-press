25994a45869bb3b1e09a1bd11a26da6ae3feb8a8
CL75 <funnotfame@gmail.com>
2015-01-11 00:45:05 -0800

    add function to modify default dashboard footer message

--- functions.php
+++ functions.php
@@ -156,6 +156,19 @@ add_filter( 'login_message', 'fp_login_message' );
 
 
 
+/* MODIFY DASHBOARD FOOTER */
+function fp_modify_dashboard_footer() {
+	
+	$local_time = date('g:i A', time()+60*60*$diff=-8);
+	
+	echo '<span id="dashboard-footer">It is currently ' . $local_time . ' on ' . date('l F jS, Y') . '. Your last login was...</span>';
+}
+add_filter('admin_footer_text', 'fp_modify_dashboard_footer');
+
+
+
+
+
 /* RESTORE LINK MANAGER THAT EXISTED IN WP < 3.5 */
 add_filter( 'pre_option_link_manager_enabled', '__return_true' );
 
@@ -195,6 +208,44 @@ function fp_remove_default_dashboard_widgets() {
   /*remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');*/
 }
 add_action( 'wp_dashboard_setup', 'fp_remove_default_dashboard_widgets' );
+
+
+
+
+
+/* ADD CUSTOM DASHBOARD WIDGETS */
+function fp_add_dashboard_widgets() {
+    wp_add_dashboard_widget( 'wptutsplus_dashboard_welcome', 'Welcome', 'fp_add_welcome_widget' );
+    wp_add_dashboard_widget( 'wptutsplus_dashboard_links', 'Useful Links', 'fp_add_links_widget' );
+}
+
+function fp_add_welcome_widget(){ ?>
+ 
+    This content management system lets you edit the pages and posts on your website.
+ 
+    Your site consists of the following content, which you can access via the menu on the left:
+ 
+    <ul>
+        <li><strong>Pages</strong> - static pages which you can edit.</li>
+        <li><strong>Posts</strong> - news or blog articles - you can edit these and add more.</li>
+        <li><strong>Media</strong> - images and documents which you can upload via the Media menu on the left or within each post or page.</li>
+    </ul>
+ 
+    On each editing screen there are instructions to help you add and edit content.
+ 
+<?php }
+ 
+function fp_add_links_widget() { ?>
+ 
+    Some links to resources which will help you manage your site:
+ 
+    <ul>
+        <li><a href="http://wordpress.org">The WordPress Codex</a></li>
+        <li><a href="http://easywpguide.com">Easy WP Guide</a></li>
+        <li><a href="http://www.wpbeginner.com">WP Beginner</a></li>
+    </ul>
+<?php }
+add_action( 'wp_dashboard_setup', 'fp_add_dashboard_widgets' );
 
 
 
